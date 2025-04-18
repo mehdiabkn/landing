95427179cf1f556fd70661b05fa6c496523aa188
unknown <abermehdi@yahoo.fr>
2025-04-19 01:06:20 +0200

    ok

--- src/components/SnapPixel.tsx
+++ src/components/SnapPixel.tsx
@@ -4,17 +4,14 @@ import { useEffect } from 'react';
 
 declare global {
   interface Window {
-    snaptr?: (...args: unknown[]) => void;
+    snaptr?: (...args: any[]) => void;
   }
 }
 
 export default function SnapPixel() {
   useEffect(() => {
-    // Si le pixel est déjà injecté, ne rien faire
-    console.log("HI MODEFO")
-    alert("Snapchazt Pixel chargé"); // ⚠️ juste pour test
+    if (window.snaptr) return;
 
-    // Injecte le script
     const script = document.createElement('script');
     script.async = true;
     script.src = 'https://sc-static.net/scevent.min.js';
@@ -24,6 +21,8 @@ export default function SnapPixel() {
 
       window.snaptr('init', '60c76ab3-21cd-4483-9b0d-6e73a38b4d50');
       window.snaptr('track', 'PAGE_VIEW');
+
+      console.log("✅ PAGE_VIEW envoyé à Snapchat mazal tov jewbuzze");
     };
 
     document.head.appendChild(script);
